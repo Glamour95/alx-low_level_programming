a4b0c5774ecaa2477f33c1a21efc02b3874689c7
Glamour <glamour.maphangat@gmail.com>
2023-02-17 04:46:42 -0800

    variables if, else and while

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,19 @@
-#!/bin/bash
-#iclude <stdio.h>
+#include <stdio.h>
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
-
+ *main - Prints the size of various types on the computer it is compiled and run on
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-         printf("size of char: zu% byte (s)\n", sizeof(char)) ;
-	 printf("size of int: zu% byte (s)\n", sizeof(int)) ;
-	 printf("size of long int: zu% byte (s)\n", sizeof(long int)) ;
-	 printf("size of long long int: zu% byte (s)\n", sizeof(long long int)) ;
-	 printf("size of a float: zu% byte (s)\n", sizeof(float)) ;
-         return (0);
+char a;
+int b,
+long int c;
+long long int d;
+float f;
+printf("size of char: %lu byte(s)\n", (unsigned long) sizeof(a));
+printf("size of int: %lu byte(s)\n", (unsigned long) sizeof(b));
+printf("size of long int: %lu byte(s)\n", (unsigned long) sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+return (0);
 }
