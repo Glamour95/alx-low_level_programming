95640b5197ad27e64a8d288db85068b9f8c42371
Glamour <glamour.maphangat@gmail.com>
2023-02-19 00:51:21 -0800

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - A program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
  *
- * Return = 0 (Success)
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	for (a = 0; a <= 10; a++)
-		putchar(a);
-	putchar('\n');
+	int i;
+	
+	/* Print all single digit numbers of base 10 */
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
 	return (0);
 }
