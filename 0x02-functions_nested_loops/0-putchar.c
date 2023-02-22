59e4fe25c589b07b427821da5f412369eb092b5c
Glamour <glamour.maphangat@gmail.com>
2023-02-21 22:52:55 -0800

    0x03-debugging

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - A program that prints _putchar.
- * Return: Always 0
+ * main - Entry point
+ *
+ * Description: This function prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
+	/* Call the _putchar function to print the string */
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,9 +19,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	_putchar('=');
-	_putchar(',');
-	_putchar('asm');
-	return (0);
 
+	/* Return 0 to indicate success */
+	return (0);
 }
