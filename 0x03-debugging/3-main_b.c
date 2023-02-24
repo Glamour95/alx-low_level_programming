18c4fbfddbecb39d0e9283694294b166a687b886
Glamour <glamour.maphangat@gmail.com>
2023-02-24 11:17:16 -0800

    3-leap year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -4,15 +4,30 @@
 /**
 * main - takes a date and prints how many days are left in the year, taking
 * leap years into account
-* Return: 0
+* 
+* Return: 0 (success).
 */
 
+/* Function to convert month and day to the day of the year */
+int convert_day(int month, int day)
+{
+	/* function implementation */
+}
+
+/* Function to print remaining days in year */
+void print_remaining_days(int month, int day, int year)
+{
+	/* function implementation */
+}
 int main(void)
 {
 	int month;
 	int day;
 	int year;
-
+	
+	day = convert_day(month, day);
+	print_remaining_days(month, day, year);
+	
 	month = 02;
 	day = 29;
 	year = 2000;
