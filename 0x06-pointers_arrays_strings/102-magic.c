24b5a31ccc58ee9fe42ef1b5564a7076d7accb1f
Glamour <glamour.maphangat@gmail.com>
2023-03-02 12:29:45 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+ * main - A program that prints a[2] = 98
+ * Return: 0 Always (success)
+ */
+
 int main(void)
 {
 	int n;
@@ -8,6 +13,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 /*
  * write your line of code here...
  * Remember:
@@ -16,7 +22,7 @@ int main(void)
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-	printf("a[2] = 98";
+	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
